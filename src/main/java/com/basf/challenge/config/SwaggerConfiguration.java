1e46759c0f2e3b01be6902f9ed9e726f31768748
Alejandrokbo <41395415+Alejandrokbo@users.noreply.github.com>
2022-12-28 19:58:10 +0100

    Initializing project structure

--- src/main/java/com/basf/challenge/config/SwaggerConfiguration.java
+++ src/main/java/com/basf/challenge/config/SwaggerConfiguration.java
@@ -5,13 +5,29 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RestController;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Collections;
+
+
 @Configuration
 public class SwaggerConfiguration {
+    private ApiInfo apiInfo() {
+        return new ApiInfo("BASF Challenge Rest APIs",
+                "APIs for MyApp.",
+                "1.0",
+                "Terms of service",
+                new Contact("Alejandro", "https://github.com/alejandrokbo", "alejandro.cabo1991@gmail.com"),
+                "License of API",
+                "API license URL",
+                Collections.emptyList());
+    }
+
     @Bean
-    public Docket api() {
+    public Docket documentation() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
